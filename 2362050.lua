cb416b556e38348e094321b661312a1a3300d030
30434 <3043452623@qq.com>
2026-02-14 20:41:26 +0800

    Add files via upload

---
+++ 2362050.lua
@@ -0,0 +1,31 @@
+-- 2362050's Lua and Manifest Created by Morrenus
+-- MY HERO ACADEMIA: All’s Justice
+-- Created: February 06, 2026 at 11:08:26 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 10
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2362050) -- MY HERO ACADEMIA: All’s Justice
+-- MAIN APP DEPOTS
+addappid(2362051, 1, "de25173640dc896951725a366eb08a505411f96b148ad83194421ec180aeb3f7") -- Depot 2362051
+setManifestid(2362051, "2438529836052475204", 14720865671)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3792210) -- MY HERO ACADEMIA Alls Justice - Playable Character Early Unlock Pack
+addappid(3792270) -- MY HERO ACADEMIA Alls Justice - 7 HUD Banners Set
+addappid(3792280) -- MY HERO ACADEMIA Alls Justice - U.A. Quest Costume Pack
+addappid(3792290) -- MY HERO ACADEMIA Alls Justice - 1-A Idol Costume Pack
+addappid(3792310) -- MY HERO ACADEMIA Alls Justice - Opera Costume Pack
+addappid(3792320) -- MY HERO ACADEMIA Alls Justice - School Uniform Himiko Toga
+addappid(3792330) -- MY HERO ACADEMIA Alls Justice - Hero Costume Lady Nagant
+addappid(3792340) -- MY HERO ACADEMIA Alls Justice - The Worldwide Star Card
+addappid(3792350) -- MY HERO ACADEMIA Alls Justice - Season Pass
+addappid(3792370) -- MY HERO ACADEMIA Alls Justice - 20 Costumes Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2362052) -- Depot 2362052 (empty depot)
