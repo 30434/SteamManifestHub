bfddf05dd95ab4782bc6e05ae862e225abb9676a
30434 <3043452623@qq.com>
2026-04-10 21:17:58 +0800

    Add files via upload

---
+++ 2567870.lua
@@ -0,0 +1,14 @@
+-- 2567870 由 方舟 生成
+-- 游戏名称: Chained Together
+-- 创建时间: 2026年03月26日 20:17:55
+-- 网址: https://steam-lua.draoon.link
+-- Depot总数: 1
+-- 免费DLC: 0 | 付费DLC: 0
+-- 共享Depot: 0
+
+-- ========== 主应用程序 ==========
+addappid(2567870) -- Chained Together
+
+-- ========== 主应用 Depot ==========
+addappid(2567871,0,"84ff26693d69b1f13f653ad7d1a182142a2a5049bf0ce8dc73735d955d3cb186") -- Depot 2567871
+-- setManifestid(2567871,"2564921531254078552")
